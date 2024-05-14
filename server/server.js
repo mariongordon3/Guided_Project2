64c12e8909b932ec03c7b75bbd5d3adf43049ad4
Mike Jones <mikejones@yahoo.com>
2024-05-14 15:29:27 -0400

    finished second half of routes

--- server/server.js
+++ server/server.js
@@ -16,18 +16,70 @@ const films_planets_collection = process.env.MONGO_DB_FILMS_PLANETS_COLLECTION;
 const app = express(); 
 const port = process.env.PORT;
 
-app.get('/api/planets', (req, res) => {
-    res.send({"test": "ok!"});
+app.get('/api/films/:id/characters', async (req, res) => {
+    try {
+                const client = await MongoClient.connect(url);
+                const db = client.db(swapi_db);
+                const collection = db.collection(films_characters_collection);
+                const {id} = req.params
+                const filmsCharacters = await collection.find({film_id:Number(id)}).toArray();
+            res.json(filmsCharacters);
+            } catch (err) {
+                console.error("Error:", err);
+            }
 });
 
-	// /api/characters
-	// /api/films
-	// /api/planets
-	// /api/characters/:id
-	// /api/films/:id
-	// /api/planets/:id
+app.get('/api/films/:id/planets', async (req, res) => {
+    try {
+                const client = await MongoClient.connect(url);
+                const db = client.db(swapi_db);
+                const collection = db.collection(films_planets_collection);
+                const {id} = req.params
+                const filmsPlanets = await collection.find({film_id:Number(id)}).toArray();
+            res.json(filmsPlanets);
+            } catch (err) {
+                console.error("Error:", err);
+            }
+});
+app.get('/api/characters/:id/films', async (req, res) => {
+    try {
+            const client = await MongoClient.connect(url);
+            const db = client.db(swapi_db);
+            const collection = db.collection(films_characters_collection);
+            const {id} = req.params
+            const characterFilms = await collection.find({character_id:Number(id)}).toArray();
+            res.json(characterFilms);
+            } catch (err) {
+                console.error("Error:", err);
+            }
+});
+app.get('/api/planets/:id/films', async (req, res) => {
+    try {
+                const client = await MongoClient.connect(url);
+                const db = client.db(swapi_db);
+                const collection = db.collection(films_planets_collection);
+                const {id} = req.params
+                const planetsFilms = await collection.find({planet_id:Number(id)}).toArray();
+            res.json(planetsFilms);
+            } catch (err) {
+                console.error("Error:", err);
+            }
+});
+app.get('/api/planets/:id/characters', async (req, res) => {
+    try {
+                const client = await MongoClient.connect(url);
+                const db = client.db(swapi_db);
+                const collection = db.collection(characters_collection);
+                const {id} = req.params
+                const planetCharacters = await collection.find({homeworld:Number(id)}).toArray();
+            res.json(planetCharacters);
+            } catch (err) {
+                console.error("Error:", err);
+            }
+});
 
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
