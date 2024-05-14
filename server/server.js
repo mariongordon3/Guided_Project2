90ecff7eb90720deb340f4585b75afa2eb387b8e
Nikki Franco <nfranco2@travelers.com>
2024-05-14 15:14:06 -0400

    .gitignore shouldn't have .env?

--- server/server.js
+++ server/server.js
@@ -12,20 +12,60 @@ const planets_collection = process.env.MONGO_DB_PLANETS_COLLECTION;
 const films_characters_collection = process.env.MONGO_DB_FILMS_CHARACTERS_COLLECTION;
 const films_planets_collection = process.env.MONGO_DB_FILMS_PLANETS_COLLECTION;
 
-
 const app = express(); 
+app.use(cors());
 const port = process.env.PORT;
 
-app.get('/api/planets', (req, res) => {
-    res.send({"test": "ok!"});
+app.get('/api/characters', async (req, res) => {
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const char_coll = db.collection(characters_collection);
+    const characters = await char_coll.find({}).toArray();
+    res.send(characters);
 });
 
-	// /api/characters
-	// /api/films
-	// /api/planets
-	// /api/characters/:id
-	// /api/films/:id
-	// /api/planets/:id
+app.get('/api/films', async (req, res) => {
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const film_coll = db.collection(films_collection);
+    const films = await film_coll.find({}).toArray();
+    res.send(films);
+});
+
+app.get('/api/planets', async (req, res) => {
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const planets_coll = db.collection(planets_collection);
+    const planets = await planets_coll.find({}).toArray();
+    res.send(planets);
+});
+
+app.get('/api/characters/:id', async (req, res) => {
+    const { id } = req.params;
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const char_coll = db.collection(characters_collection);
+    const character = await char_coll.find({"id": Number(id)}).toArray();
+    res.send(character);
+});
+
+app.get('/api/films/:id', async (req, res) => {
+    const { id } = req.params;
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const films_coll = db.collection(films_collection);
+    const film = await films_coll.find({"id": Number(id)}).toArray();
+    res.send(film);
+});
+
+app.get('/api/planets/:id', async (req, res) => {
+    const { id } = req.params;
+    const client = await MongoClient.connect(url);
+    const db = client.db(swapi_db);
+    const planets_coll = db.collection(planets_collection);
+    const planet = await planets_coll.find({"id": Number(id)}).toArray();
+    res.send(planet);
+});
 
 
 app.listen(port, () => {
