bd8c086ba48574c0dd6b106666f02a03eb22a9d1
Mike Jones <mikejones@yahoo.com>
2024-05-14 16:50:51 -0400

    started react

---
+++ react/src/components/Characters.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import React from "react"
+
+const Characters = (props)=>{
+const {data} = props
+    return(
+        <div>
+
+        </div>
+    )
+} 
+
+export default Characters
