fabace710b6db68d8c1f95f808ca86b433f847c2
Nikki Franco <nfranco2@travelers.com>
2024-05-14 15:30:10 -0400

    added try/catch blocks to first six routes

--- server/server.js
+++ server/server.js
@@ -17,54 +17,84 @@ app.use(cors());
 const port = process.env.PORT;
 
 app.get('/api/characters', async (req, res) => {
-    const client = await MongoClient.connect(url);
-    const db = client.db(swapi_db);
-    const char_coll = db.collection(characters_collection);
-    const characters = await char_coll.find({}).toArray();
-    res.send(characters);
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(swapi_db);
+        const char_coll = db.collection(characters_collection);
+        const characters = await char_coll.find({}).toArray();
+        res.status(200).send(characters);
+    } catch (err) { 
+        console.log('Error: ', err);
+        res.status(500).send('Could not fetch characters!');
+    }
 });
 
 app.get('/api/films', async (req, res) => {
-    const client = await MongoClient.connect(url);
-    const db = client.db(swapi_db);
-    const film_coll = db.collection(films_collection);
-    const films = await film_coll.find({}).toArray();
-    res.send(films);
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(swapi_db);
+        const film_coll = db.collection(films_collection);
+        const films = await film_coll.find({}).toArray();
+        res.status(200).send(films);
+    } catch (err) { 
+        console.log('Error: ' , err);
+        res.status(500).send('Could not fetch films!');
+    }
 });
 
 app.get('/api/planets', async (req, res) => {
-    const client = await MongoClient.connect(url);
-    const db = client.db(swapi_db);
-    const planets_coll = db.collection(planets_collection);
-    const planets = await planets_coll.find({}).toArray();
-    res.send(planets);
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(swapi_db);
+        const planets_coll = db.collection(planets_collection);
+        const planets = await planets_coll.find({}).toArray();
+        res.status(200).send(planets);
+    } catch (err) { 
+        console.log('Error: ', err);
+        res.status(500).send('Could not fetch planets!');
+    }
 });
 
 app.get('/api/characters/:id', async (req, res) => {
-    const { id } = req.params;
-    const client = await MongoClient.connect(url);
-    const db = client.db(swapi_db);
-    const char_coll = db.collection(characters_collection);
-    const character = await char_coll.find({"id": Number(id)}).toArray();
-    res.send(character);
+    try {
+        const { id } = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(swapi_db);
+        const char_coll = db.collection(characters_collection);
+        const character = await char_coll.find({"id": Number(id)}).toArray();
+        res.status(200).send(character);
+    } catch (err) { 
+        console.log('Error: ', err);
+        res.status(500).send('Could not fetch character!')
+    }
 });
 
 app.get('/api/films/:id', async (req, res) => {
+    try {
     const { id } = req.params;
     const client = await MongoClient.connect(url);
     const db = client.db(swapi_db);
     const films_coll = db.collection(films_collection);
     const film = await films_coll.find({"id": Number(id)}).toArray();
-    res.send(film);
+    res.status(200).send(film);
+    } catch (err) { 
+        console.log('Error: ', err);
+        res.status(500).send('Could not fetch film!')
+    }
 });
 
 app.get('/api/planets/:id', async (req, res) => {
-    const { id } = req.params;
-    const client = await MongoClient.connect(url);
-    const db = client.db(swapi_db);
-    const planets_coll = db.collection(planets_collection);
-    const planet = await planets_coll.find({"id": Number(id)}).toArray();
-    res.send(planet);
+    try {
+        const { id } = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(swapi_db);
+        const planets_coll = db.collection(planets_collection);
+        const planet = await planets_coll.find({"id": Number(id)}).toArray();
+        res.status(200).send(planet);
+    } catch (err) {
+        console.log('Error: ', err);
+        res.status(500).send('Could not fetch planet!');
+    }
 });
 
 
