59d7aa5c5e2cec0504ac2cdc2ddf2f3010c064a5
Nikki Franco <nfranco2@travelers.com>
2024-05-15 10:04:36 -0400

    continued characters.jsx

--- react/src/components/Characters.jsx
+++ react/src/components/Characters.jsx
@@ -1,14 +1,34 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import React from "react"
+import React, { useState, useEffect } from "react";
 
-const Characters = (props)=>{
-const {data} = props
-    return(
-        <div>
+const Characters = (props) => {
+    const [characters, setCharacters] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_SWAPI_API_URL}/characters`);
+                if (!response.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                const json_response = await response.json();
+                console.log(json_response)
+                setCharacters(json_response); // assign JSON response to the data variable.
 
-        </div>
-    )
-} 
+            } catch (error) {
+                console.error('Error fetching socks:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <section className="characterList">
+            {
+                characters.map((char) => (
+                    <div key={char.id}>{(char.name)}</div>
+                ))
+            }
+        </section>
+    );
+};
 
 export default Characters
